d45c7a20cd4d0dea70df691ef496d460adaa4414
Stan Svec <stan@taggstar.com>
2019-08-27 12:55:49 +0100

    common parser

--- taro/app.py
+++ taro/app.py
@@ -1,5 +1,5 @@
-import sys
 import argparse
+import sys
 
 from taro import runner
 from taro.job import Job
@@ -8,18 +8,24 @@ from taro.process import ProcessExecution
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description='Manage your jobs with Taro')
-    parser.add_argument('--debug', action='store_true')
-
     subparsers = parser.add_subparsers(dest='command')
-    exec_parser = subparsers.add_parser('exec')
-    exec_parser.add_argument('-t', '--timeout', type=int)
+
+    common = argparse.ArgumentParser()
+    common.add_argument('--debug', action='store_true')
+
+    exec_parser = subparsers.add_parser('exec', parents=[common], description='Execute command', add_help=False)
     exec_parser.add_argument('--id', type=str, default='anonymous')
+    exec_parser.add_argument('-t', '--timeout', type=int)
     exec_parser.add_argument('args', type=str, nargs=argparse.REMAINDER, help='an execution argument')
+
     return parser.parse_args(args)
 
 
 def main(args):
     args = parse_args(args)
+    print(args)
+    if args.debug:
+        print('debug on')
     if args.command == 'exec':
         run_exec(args)
 
