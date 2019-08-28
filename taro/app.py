f091251b19f514d85d7b1f75a6484ab62cbc8ccf
Stan Svec <stan@taggstar.com>
2019-08-28 13:42:51 +0100

    logging configuration

--- taro/app.py
+++ taro/app.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import sys
 
 from taro import runner
@@ -11,7 +12,7 @@ def parse_args(args):
     subparsers = parser.add_subparsers(dest='command')
 
     common = argparse.ArgumentParser()
-    common.add_argument('--debug', action='store_true')
+    common.add_argument('--log-level', type=str, default=logging.INFO)
 
     exec_parser = subparsers.add_parser('exec', parents=[common], description='Execute command', add_help=False)
     exec_parser.add_argument('--id', type=str, default='anonymous')
@@ -23,11 +24,17 @@ def parse_args(args):
 
 def main(args):
     args = parse_args(args)
-    print(args)
-    if args.debug:
-        print('debug on')
+    configure_logging(args)
+
     if args.command == 'exec':
         run_exec(args)
+
+
+def configure_logging(args):
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=args.log_level,
+        format='%(asctime)s - %(levelname)-5s - %(name)s - %(message)s')
 
 
 def run_exec(args):
