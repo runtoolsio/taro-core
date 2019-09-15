290c220cd175db9156a9d99e301f7b23a2b04a35
Stan Svec <stan@taggstar.com>
2019-09-15 23:52:33 +0100

    login setup simpler

--- taro/app.py
+++ taro/app.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import sys
 
 from taro import log
@@ -10,32 +9,43 @@ from taro.process import ProcessExecution
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description='Manage your jobs with Taro')
-    subparsers = parser.add_subparsers(dest='action')
-
     common = argparse.ArgumentParser()
-    common.add_argument('--log-level', type=str, default=logging.INFO)  # Remove default
-    # https://bugs.python.org/issue14074
-    common.add_argument('--log', nargs='+', action='append', type=str, metavar='<arg>',
-                        help='<logger> [level] [arg] ...', default=[])
+    subparsers = parser.add_subparsers(dest='action')  # command/action
 
     exec_parser = subparsers.add_parser('exec', parents=[common], description='Execute command', add_help=False)
-    exec_parser.add_argument('--id', type=str, default='anonymous')
-    exec_parser.add_argument('-t', '--timeout', type=int)
-    exec_parser.add_argument('-c', '--command', type=str, nargs='+', help='an execution argument', required=True)
+    # exec_parser.add_argument('--log', nargs='+', action='append', type=str, metavar='<arg>',
+    #                          help='<logger> [level] [arg] ...', default=[])
+    # exec_parser.add_argument('--log-level', type=str, default=logging.INFO)  # Remove default
+    # exec_parser.add_argument('-t', '--timeout', type=int)
+    # # Terms command and arguments taken from python doc and docker run help,
+    # # for this app these are operands (alternatively arguments)
+    exec_parser.add_argument('--dry-run', action='store_true')  # TODO
+    exec_parser.add_argument('--id', type=str, default='anonymous', help='job ID')
+    exec_parser.add_argument('--log-file', nargs=1, type=str, metavar='<log-level>',
+                             help='log into taro.log file with given <log-level>')
+    # Terms command and arguments taken from python doc and docker run help,
+    # for this app (or rather exec command) these are operands (alternatively arguments)
+    exec_parser.add_argument('command', type=str, metavar='COMMAND', help='program to execute')
+    exec_parser.add_argument('arg', type=str, metavar='ARG', nargs=argparse.REMAINDER, help="program arguments")
 
     return parser.parse_args(args)
 
 
 def main(args):
     args = parse_args(args)
-    log.configure(args)
+    setup_logging(args)
 
     if args.action == 'exec':
         run_exec(args)
 
 
+def setup_logging(args):
+    if args.log_file and args.log_file[0].lower() != 'off':
+        log.setup_file(args.log_file[0].lower())
+
+
 def run_exec(args):
-    execution = ProcessExecution(args.command)
+    execution = ProcessExecution([args.command] + args.arg)
     job = Job(args.id, execution)
     runner.run(job)
 
--- taro/log.py
+++ taro/log.py
@@ -1,4 +1,3 @@
-import argparse
 import logging
 import pathlib
 
@@ -7,44 +6,23 @@ _root_logger.setLevel(logging.DEBUG)
 _formatter = logging.Formatter('%(asctime)s - %(levelname)-5s - %(name)s - %(message)s')
 
 
-def configure(args):
-    for values in args.log:
-        log_type = values[0].lower()
-        level = values[1] if len(values) > 1 else None
-
-        if 'file' == log_type:
-            _raise_error_on_unknown_values(values, 3)
-
-            file = values[2] if len(values) > 2 else _get_log_file_path()
-            _configure_file(level, file)
-        else:
-            _raise_error_on_unknown_values(values, 2)
-
-            if 'stdout' == log_type:
-                _configure_console(level)
-
-
-def _raise_error_on_unknown_values(values, max_values):
-    if len(values) > max_values:
-        raise argparse.ArgumentError(None, "Argument --log has unknown values: " + ' '.join(values[max_values:]))
-
-
-def _get_log_file_path():
-    home = pathlib.Path.home()
-    path = home / '.cache' / 'taro'
-    path.mkdir(parents=True, exist_ok=True)
-    return str(path / 'taro.log')
-
-
-def _configure_console(level):
+def _setup_console(level):
     console_handler = logging.StreamHandler()
     console_handler.setLevel(level or logging.WARN)
     console_handler.setFormatter(_formatter)
     _root_logger.addHandler(console_handler)
 
 
-def _configure_file(level, file_path):
-    file_handler = logging.FileHandler(file_path)
-    file_handler.setLevel(level or logging.INFO)
+def setup_file(level):
+    file_handler = logging.FileHandler(_get_log_file_path())
+    file_handler.setLevel(logging.getLevelName(level.upper()))
     file_handler.setFormatter(_formatter)
     _root_logger.addHandler(file_handler)
+
+
+def _get_log_file_path():
+    """TODO root"""
+    home = pathlib.Path.home()
+    path = home / '.cache' / 'taro'
+    path.mkdir(parents=True, exist_ok=True)
+    return str(path / 'taro.log')
