54893e156be965736bde72a0018269b211fc7cda
Stan Svec <stan@taggstar.com>
2020-03-02 11:28:45 +0000

    stop test working

--- test/test_app_stop.py
+++ test/test_app_stop.py
@@ -4,18 +4,22 @@ Command: stop
 """
 from multiprocessing.context import Process
 
+from taro.execution import ExecutionState
 from test.util import run_app
 
 
 def test_more_jobs_require_all_flag(capsys):
-    p1 = Process(target=run_app, args=('exec --id j1 sleep 5',))
+    pw = Process(target=run_app, args=('exec wait -c 2 ' + ExecutionState.RUNNING.name,))
+    pw.start()
+    p1 = Process(target=run_app, args=('exec --id j1 sleep 5',), daemon=True)
     p1.start()
-    p2 = Process(target=run_app, args=('exec --id j1 sleep 5',))
+    p2 = Process(target=run_app, args=('exec --id j1 sleep 5',), daemon=True)
     p2.start()
 
+    pw.join()  # Wait for both exec to run
     run_app('stop j1')
 
     output = capsys.readouterr().out
-    assert 'The criteria matches more than one job' in output
+    assert 'more than one job' in output
     assert p1.is_alive()
     assert p2.is_alive()
