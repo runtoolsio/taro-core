03032ac247ee5a601478f56946cd4798781f2538
stan svec <stan.x.svec@gmail.com>
2020-04-23 11:17:09 +0100

    taro package public api

---
+++ taro/__init__.py
@@ -0,0 +1,6 @@
+"""
+Public API of this package is imported here and it is safe to use by plugins.
+Any API in sub-modules (except 'util' module) is a subject to change and doesn't ensure backwards compatibility.
+"""
+from .execution import ExecutionStateGroup, ExecutionState, ExecutionError, ExecutionLifecycle
+from .job import JobInstance, ExecutionStateObserver
