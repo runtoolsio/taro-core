0b2dd60e4992b047785914310209f61a9f741c83
Stan Svec <stan@taggstar.com>
2020-04-26 19:09:59 +0100

    more plugin tests

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,6 +1,6 @@
-import abc
 from typing import Optional
 
+import test_plugin
 from taro import plugins, PluginBase, JobControl
 
 
@@ -14,6 +14,26 @@ def test_invalid_plugin_ignored():
     """Test that error raised during plugin import is captured"""
     name2module = plugins.discover_ext_plugins('test_', ['test_plugin_invalid'])
     assert len(name2module) == 0
+
+
+def test_create_plugins():
+    name2plugin = plugins.PluginBase.create_plugins('test_', ['plugin2', 'test_plugin'])
+    assert len(name2plugin) == 2
+    assert isinstance(name2plugin['plugin2'], Plugin2)
+    assert isinstance(name2plugin['test_plugin'], test_plugin.TestPlugin)
+
+
+def test_non_existing_plugin_ignored():
+    name2plugin = plugins.PluginBase.create_plugins('none', ['plugin2', 'plugin4'])
+    assert len(name2plugin) == 1
+    assert isinstance(name2plugin['plugin2'], Plugin2)
+
+
+def test_create_invalid_plugins():
+    Plugin2.error_on_init = BaseException('Must be caught')
+    name2plugin = plugins.PluginBase.create_plugins('none', ['plugin2', 'plugin3'])
+    assert len(name2plugin) == 1
+    assert isinstance(name2plugin['plugin3'], Plugin3)
 
 
 class Plugin2(PluginBase, plugin_name='plugin2'):
