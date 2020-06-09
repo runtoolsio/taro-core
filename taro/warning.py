faa4034372223f94fd8e67da0702099f9b0f9172
Stan Svec <stan@taggstar.com>
2020-06-09 08:44:38 +0100

    warning check abc

--- taro/warning.py
+++ taro/warning.py
@@ -1,5 +1,6 @@
 import abc
 from collections import namedtuple
+from threading import Event, Thread
 
 from taro import JobInfo
 
@@ -15,3 +16,41 @@ class JobWarningObserver(abc.ABC):
     @abc.abstractmethod
     def warning_removed(self, job_info: JobInfo, warning: Warn):
         """This method is called when job instance execution state is changed."""
+
+
+class WarningCheck(abc.ABC):
+
+    def next_check(self, job_instance) -> int:
+        """
+        Returns maximum time in seconds after next check must be performed.
+        However next check can be performed anytime sooner than after then interval specified by this method.
+
+        :param job_instance: checked job
+        :return: next latest check
+        """
+
+    def check(self, job_instance):
+        """
+        Check warning condition and return Warn object if the warning condition is met otherwise return None.
+
+        :param job_instance: checked job
+        :return: warning or None
+        """
+
+
+class WarningChecking:
+
+    def __init__(self, job_control, *warning):
+        self._job_control = job_control
+        self._warnings = tuple(*warning)
+        self._wait = Event()
+        self._checker = Thread(target=self._run, name='Warning-Checker', daemon=True)
+
+    def _run(self):
+        next_check = 1
+        for warning in self._warnings:
+            warn = warning.check(self._job_control)
+            if warn:
+                self._job_control.add_warning(warn)
+            next_check = min(next_check, warning.next_check(self._job_control))
+        self._wait.wait(next_check)
