62be998bd38e90a735bc983ed2224682ea382186
Stan Svec <stan@taggstar.com>
2020-08-12 13:54:18 +0100

    output dispatcher/receiver

--- taro/listening.py
+++ taro/listening.py
@@ -1,15 +1,14 @@
 #  Sender, Listening
 from taro import util, dto
-from taro.job import ExecutionStateObserver, JobInfo
+from taro.job import ExecutionStateObserver, JobInfo, JobOutputObserver
 from taro.socket import SocketServer, SocketClient
-from taro.util import iterates
 
 STATE_LISTENER_FILE_EXTENSION = '.slistener'
 OUTPUT_LISTENER_FILE_EXTENSION = '.olistener'
 
 
-def _create_state_listener_socket_name():
-    return util.unique_timestamp_hex() + STATE_LISTENER_FILE_EXTENSION
+def _listener_socket_name(ext):
+    return util.unique_timestamp_hex() + ext
 
 
 class StateDispatcher(ExecutionStateObserver):
@@ -17,9 +16,8 @@ class StateDispatcher(ExecutionStateObserver):
     def __init__(self):
         self._client = SocketClient(STATE_LISTENER_FILE_EXTENSION, bidirectional=False)
 
-    @iterates
     def state_update(self, job_info: JobInfo):
-        event_body = {"event_type": "job_state_change", "event": {"job_info": dto.to_info_dto(job_info)}}
+        event_body = {"event_type": "execution_state_change", "event": {"job_info": dto.to_info_dto(job_info)}}
         self._client.communicate(event_body)
 
     def close(self):
@@ -29,10 +27,36 @@ class StateDispatcher(ExecutionStateObserver):
 class StateReceiver(SocketServer):
 
     def __init__(self):
-        super().__init__(_create_state_listener_socket_name())
+        super().__init__(_listener_socket_name(STATE_LISTENER_FILE_EXTENSION))
         self.listeners = []
 
     def handle(self, req_body):
         job_info = dto.to_job_info(req_body['event']['job_info'])
         for listener in self.listeners:
             listener.state_update(job_info)
+
+
+class OutputDispatcher(JobOutputObserver):
+
+    def __init__(self):
+        self._client = SocketClient(OUTPUT_LISTENER_FILE_EXTENSION, bidirectional=False)
+
+    def output_update(self, job_info: JobInfo, output):
+        event_body = {"event_type": "new_output", "event": {"job_info": dto.to_info_dto(job_info), "output": output}}
+        self._client.communicate(event_body)
+
+    def close(self):
+        self._client.close()
+
+
+class OutputReceiver(SocketServer):
+
+    def __init__(self):
+        super().__init__(_listener_socket_name(OUTPUT_LISTENER_FILE_EXTENSION))
+        self.listeners = []
+
+    def handle(self, req_body):
+        job_info = dto.to_job_info(req_body['event']['job_info'])
+        output = req_body['event']['output']
+        for listener in self.listeners:
+            listener.output_update(job_info, output)
