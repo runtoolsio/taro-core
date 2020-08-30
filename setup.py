2d40eace6aba170020592d6e11bd6034572281a1
stan svec <stan.x.svec@gmail.com>
2020-08-30 08:22:49 +0100

    outputable execution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="taro",
-    version="0.0.10",
+    version="0.0.11",
     author="Stan Svec",
     author_email="stan.x.svec@gmail.com",
     description="Tool for managing your (mainly cron) jobs",
