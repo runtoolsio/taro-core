b7b82e6d07cb00e93296c2d49ed9ff569ac2d6f5
Stan Svec <stan@taggstar.com>
2020-09-06 10:19:06 +0100

    no instance to stop handled

--- taro/cmd/stop.py
+++ taro/cmd/stop.py
@@ -2,18 +2,22 @@ import os
 
 from taro import ps
 from taro.api import Client
-from taro.view import instance as view_inst
+from taro.view.instance import JOB_ID, INSTANCE_ID, CREATED, STATE
 
 
-# TODO no instances to stop
 def run(args):
     with Client() as client:
         all_jobs = client.read_jobs_info()
         jobs = [job for job in all_jobs if job.matches(args.instance)]
+
+        if not jobs:
+            print('No such instance to stop: ' + args.instance)
+            exit(1)
+
         if len(jobs) > 1 and not args.all:
-            print('No action performed, because the criteria matches more than one job.'
+            print('No action performed, because the criteria matches more than one instance. '
                   'Use --all flag if you wish to stop them all:' + os.linesep)
-            ps.print_table(jobs, view_inst.DEFAULT_COLUMNS, show_header=True, pager=False)
+            ps.print_table(jobs, [JOB_ID, INSTANCE_ID, CREATED, STATE], show_header=True, pager=False)
             return  # Exit code non-zero?
 
         inst_results = client.stop_jobs([job.instance_id for job in jobs], args.interrupt)
