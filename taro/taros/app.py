a5dc0cc10ec4436f2f43fffd028e30b8c72b9d48
stan svec <stan.x.svec@gmail.com>
2020-09-20 12:59:51 +0100

    taros instances

--- taro/taros/app.py
+++ taro/taros/app.py
@@ -1,18 +1,23 @@
-from bottle import route, run, response
+from bottle import route, run
 
 from taro import client, dto
 
 
 @route('/instances')
-def hello():
-    return {
-        "_links": {
-            "self": "/instances"
-        },
-        "_embedded": {
-            "instances": [dto.to_info_dto(i) for i in client.read_jobs_info()]
-        }
-    }
+def instances():
+    embedded = {"instances": [resource(dto.to_info_dto(i), links={"self": "/instances/" + i.instance_id})
+                              for i in client.read_jobs_info()]}
+    return resource({}, links={"self": "/instances"}, embedded=embedded)
+
+
+def resource(props, *, links=None, embedded=None):
+    res = {}
+    if links:
+        res["_links"] = links
+    if embedded:
+        res["_embedded"] = embedded
+    res.update(props)
+    return res
 
 
 run(host='localhost', port=8080, debug=True, reloader=True)
