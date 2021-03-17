57b6d65ebddf80c935a24f4a18bedce60bbf123c
Stan Svec <stan@taggstar.com>
2021-03-17 12:53:53 +0000

    test process failures

--- test/test_process.py
+++ test/test_process.py
@@ -3,18 +3,40 @@ Tests :mod:`process` module
 """
 from multiprocessing import Pipe
 
-from taro import ExecutionState
+import pytest
+
+from taro import ExecutionState, ExecutionError
 from taro.process import ProcessExecution
 
 
 def test_exec():
     parent, child = Pipe()
-    e = ProcessExecution(exec_, (child,))
+    e = ProcessExecution(exec_hello, (child,))
     term_state = e.execute()
     assert parent.recv() == ['hello']
     assert term_state == ExecutionState.COMPLETED
 
 
-def exec_(pipe):
+def exec_hello(pipe):
     pipe.send(['hello'])
     pipe.close()
+
+
+def test_failure_error():
+    e = ProcessExecution(exec_failure_error, ())
+    with pytest.raises(ExecutionError):
+        e.execute()
+
+
+def exec_failure_error():
+    raise AssertionError
+
+
+def test_failure_exit():
+    e = ProcessExecution(exec_failure_exit, ())
+    with pytest.raises(ExecutionError):
+        e.execute()
+
+
+def exec_failure_exit():
+    exit(1)
