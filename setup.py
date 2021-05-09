7eafc806900151bc4e8e344b8381763c8b2729ce
Stan Svec <stan@taggstar.com>
2021-05-09 04:55:38 +0100

    setup.py updated

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="taro",
-    version="0.0.12",
+    version="0.1.0",
     author="Stan Svec",
-    author_email="stan.x.svec@gmail.com",
-    description="Tool for managing your (mainly cron) jobs",
+    author_email="dev@stansvec.com",
+    description="Tool for managing your (cron) jobs",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/StanSvec/taro",
+    url="https://github.com/taro-suite/taro",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers, Ops, Admins",
@@ -23,13 +23,12 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
     ],
     python_requires='>=3.6',
-    packages=find_packages(include=("taro", "taroapp", "taros")) + find_namespace_packages(include=("taro.db",)),
+    packages=find_packages(exclude=("test*",)) + find_namespace_packages(include=("taro.db",)),
     install_requires=[
         "PyYAML>=5.1.2",
         "pypager>=3.0.0",
         "bottle>=0.12.18",
         "urllib3>=1.26.2",
-        "python-daemon>=2.2.4",
     ],
     package_data={
         'taro': ['config/*.yaml'],
