5cd43cd293fe0959c9e26e7d849ab494f5a2be26
Stan Svec <stan@taggstar.com>
2021-06-09 22:09:43 +0100

    v0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="taro",
-    version="0.1.0",
+    version="0.1.2",
     author="Stan Svec",
     author_email="dev@stansvec.com",
     description="Tool for managing your (cron) jobs",
