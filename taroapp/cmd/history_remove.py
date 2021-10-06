396408cf5b2a5bb108ba06b4f9c37ec9faf9b5ec
Petr Sixta <petr.sixt@gmail.com>
2021-10-06 17:08:51 +0200

    improved history-remove

--- taroapp/cmd/history_remove.py
+++ taroapp/cmd/history_remove.py
@@ -5,7 +5,7 @@ from taroapp import cli
 def run(args):
     print("Jobs to be removed:")  
     for id in args.id:
-        print(id)
+        print(str(persistence.num_of_job(id_=id)) + " records found for " + id)
     print("Do you want to continue? [Y/n] ", end="")  
     i = input()
     if i in cli._true_options:
