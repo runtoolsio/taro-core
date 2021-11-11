e8b3ba46ac51403949a1f6d505232d5266aa7b31
stan svec <stan.x.svec@gmail.com>
2021-11-11 12:48:05 +0000

    all columns red for failed jobs

--- taroapp/view/instance.py
+++ taroapp/view/instance.py
@@ -4,7 +4,9 @@ from taro.theme import Theme
 from taroapp.printer import Column, format_dt
 
 
-def no_style(_):
+def general_style(job):
+    if job.state.is_failure():
+        return Theme.state_failure
     return ""
 
 
@@ -26,16 +28,16 @@ def state_style(job):
     return ""
 
 
-JOB_ID = Column('JOB ID', 30, lambda j: j.job_id, no_style)
-INSTANCE_ID = Column('INSTANCE ID', 23, lambda j: j.instance_id, no_style)
-CREATED = Column('CREATED', 25, lambda j: format_dt(j.lifecycle.changed(ExecutionState.CREATED)), no_style)
-EXECUTED = Column('EXECUTED', 25, lambda j: format_dt(j.lifecycle.execution_started()), no_style)
-ENDED = Column('ENDED', 25, lambda j: format_dt(j.lifecycle.execution_finished()), no_style)
-EXEC_TIME = Column('EXECUTION TIME', 18, lambda j: execution_time_str(j), no_style)
+JOB_ID = Column('JOB ID', 30, lambda j: j.job_id, general_style)
+INSTANCE_ID = Column('INSTANCE ID', 23, lambda j: j.instance_id, general_style)
+CREATED = Column('CREATED', 25, lambda j: format_dt(j.lifecycle.changed(ExecutionState.CREATED)), general_style)
+EXECUTED = Column('EXECUTED', 25, lambda j: format_dt(j.lifecycle.execution_started()), general_style)
+ENDED = Column('ENDED', 25, lambda j: format_dt(j.lifecycle.execution_finished()), general_style)
+EXEC_TIME = Column('EXECUTION TIME', 18, lambda j: execution_time_str(j), general_style)
 STATE = Column('STATE', max(len(s.name) for s in ExecutionState) + 2, lambda j: j.state.name, state_style)
 WARNINGS = Column('WARNINGS', 40, lambda j: ', '.join("{}: {}".format(k, v) for k, v in j.warnings.items()), warn_style)
-STATUS = Column('STATUS', 50, lambda j: j.status or '', no_style)
-RESULT = Column('RESULT', 50, lambda j: j.status or '', no_style)
+STATUS = Column('STATUS', 50, lambda j: j.status or '', general_style)
+RESULT = Column('RESULT', 50, lambda j: j.status or '', general_style)
 
 DEFAULT_COLUMNS = [JOB_ID, INSTANCE_ID, CREATED, EXEC_TIME, STATE, WARNINGS, STATUS]
 
