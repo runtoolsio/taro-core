05c9971a3767381babaf9a10e4269a3248da006b
Petr Sixta <petr.sixt@gmail.com>
2022-01-22 23:09:49 +0100

    history yesterday #46

--- taroapp/cmd/history.py
+++ taroapp/cmd/history.py
@@ -22,6 +22,8 @@ def _build_job_filter(args):
         job_filter <<= jfilter.create_id_filter(args.id)
     if args.today:
         job_filter <<= jfilter.today_filter
+    if args.yesterday:
+        job_filter <<= jfilter.yesterday_filter
     if args.since:
         job_filter <<= jfilter.create_since_filter(args.since)
     if args.until:
