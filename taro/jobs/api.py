197d6ce9042bd9076f37efcae25f5e3dae87235c
stan svec <stan.x.svec@gmail.com>
2022-01-30 15:43:32 +0000

    api refactoring

--- taro/jobs/api.py
+++ taro/jobs/api.py
@@ -1,5 +1,4 @@
 import logging
-from typing import Tuple
 
 from taro import dto
 from taro.socket import SocketServer
@@ -13,16 +12,6 @@ def _create_socket_name(job_info):
     return job_info.instance_id + API_FILE_EXTENSION
 
 
-def _resp(code: int, job_instance: Tuple[str, str], data):
-    return {"resp": {"code": code}, "job_id": job_instance[0], "instance_id": job_instance[1], "data": data}
-
-
-def _resp_err(code: int, job_instance: Tuple[str, str], error: str):
-    if 400 > code >= 600:
-        raise ValueError("Error code must be 4xx or 5xx")
-    return {"resp": {"code": code}, "job_id": job_instance[0], "instance_id": job_instance[1], "error": error}
-
-
 class Server(SocketServer):
 
     def __init__(self, job_instance, latch_release):
@@ -31,42 +20,58 @@ class Server(SocketServer):
         self._latch_release = latch_release
 
     def handle(self, req_body):
-        job_inst = (self._job_instance.job_id, self._job_instance.instance_id)
-
         if 'req' not in req_body:
-            return _resp_err(422, job_inst, "missing_req")
+            return self._resp_err(422, "missing_req")
         if 'api' not in req_body['req']:
-            return _resp_err(422, job_inst, "missing_req_api")
+            return self._resp_err(422, "missing_req_api")
 
         job_instance_filter = req_body.get('job_instance')
         if job_instance_filter and not self._job_instance.create_info().matches(job_instance_filter):
-            return _resp(412, job_inst, {"reason": "job_instance_not_matching"})
+            return self._resp(412, {"reason": "job_instance_not_matching"})
 
         if req_body['req']['api'] == '/job':
             info_dto = dto.to_info_dto(self._job_instance.create_info())
-            return _resp(200, job_inst, {"job_info": info_dto})
+            return self._resp(200, {"job_info": info_dto})
 
         if req_body['req']['api'] == '/release':
             if 'data' not in req_body:
-                return _resp_err(422, job_inst, "missing_data")
+                return self._resp_err(422, "missing_data")
             if 'pending' not in req_body['data']:
-                return _resp_err(422, job_inst, "missing_data_field:pending")
+                return self._resp_err(422, "missing_data_field:pending")
 
             if self._latch_release:
                 released = self._latch_release.release(req_body.get('data').get('pending'))
             else:
                 released = False
-            return _resp(200, job_inst, {"released": released})
+            return self._resp(200, {"released": released})
 
         if req_body['req']['api'] == '/stop':
             self._job_instance.stop()
-            return _resp(200, job_inst, {"result": "stop_performed"})
+            return self._resp(200, {"result": "stop_performed"})
 
         if req_body['req']['api'] == '/interrupt':
             self._job_instance.interrupt()
-            return _resp(200, job_inst, {"result": "interrupt_performed"})
+            return self._resp(200, {"result": "interrupt_performed"})
 
         if req_body['req']['api'] == '/tail':
-            return _resp(200, job_inst, {"tail": self._job_instance.last_output})
+            return self._resp(200, {"tail": self._job_instance.last_output})
 
-        return _resp_err(404, job_inst, "req_api_not_found")
+        return self._resp_err(404, "req_api_not_found")
+
+    def _resp(self, code: int, data):
+        return {
+            "resp": {"code": code},
+            "job_id": self._job_instance.job_id,
+            "instance_id": self._job_instance.instance_id,
+            "data": data
+        }
+
+    def _resp_err(self, code: int, error: str):
+        if 400 > code >= 600:
+            raise ValueError("Error code must be 4xx or 5xx")
+        return {
+            "resp": {"code": code},
+            "job_id": self._job_instance.job_id,
+            "instance_id": self._job_instance.instance_id,
+            "error": error
+        }
