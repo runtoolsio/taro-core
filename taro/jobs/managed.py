c32654b238df7084fdc0bacaa4f73816bf28baad
Stan Svec <stan@taggstar.com>
2022-11-21 10:47:32 +0700

    managed job context

--- taro/jobs/managed.py
+++ taro/jobs/managed.py
@@ -1,6 +1,7 @@
 import logging
 
-from taro import cfg
+from taro import cfg, JobInstance
+from taro.err import InvalidStateError
 from taro.jobs import lock, plugins
 from taro.jobs.api import Server
 from taro.jobs.events import StateDispatcher, OutputDispatcher
@@ -9,6 +10,52 @@ from taro.jobs.runner import RunnerJobInstance
 log = logging.getLogger(__name__)
 
 EXT_PLUGIN_MODULE_PREFIX = plugins.DEF_PLUGIN_MODULE_PREFIX
+
+
+class ManagedJobContext:
+
+    def __init__(self,
+                 api_factory=Server,
+                 state_dispatcher_factory=StateDispatcher,
+                 output_dispatcher_factory=OutputDispatcher):
+        self._api = api_factory()
+        self._state_dispatcher = state_dispatcher_factory()
+        self._output_dispatcher = output_dispatcher_factory()
+        self._opened = False
+        self._closed = False
+
+    def __enter__(self):
+        if self._opened:
+            raise InvalidStateError("Managed job context has been already opened")
+
+        if self._api:
+            api_started = self._api.start()  # Starts a new thread
+            if not api_started:
+                raise InvalidStateError("API server failed to start")
+
+        self._opened = True
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._closed:
+            return
+
+        self._closed = True
+
+    def create_job(self, job_id, execution, state_locker=lock.default_state_locker(), *,
+                   no_overlap=False, depends_on=None, pending_value=None, **params) -> JobInstance:
+        # TODO instance_id
+        job_instance = \
+            RunnerJobInstance(job_id, execution, state_locker,
+                              pending_value=pending_value, no_overlap=no_overlap, depends_on=depends_on, **params)
+        if self._state_dispatcher:
+            job_instance.add_state_observer(self._state_dispatcher)
+        if self._output_dispatcher:
+            job_instance.add_output_observer(self._output_dispatcher)
+        if self._api:
+            self._api.add_job_instance(job_instance)
+
+        return job_instance
 
 
 def create_managed_job(job_id, execution, state_locker=lock.default_state_locker(), *,
