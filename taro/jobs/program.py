06c72416e4e0c702e4992e9f4e926d132e12e5bd
Stan Svec <stan@taggstar.com>
2023-01-19 11:01:38 +0700

    #64: separately processing stdout and stderr in program

--- taro/jobs/program.py
+++ taro/jobs/program.py
@@ -1,13 +1,8 @@
-"""
-TODO: Preserve stderr
- - https://stackoverflow.com/questions/31833897/python-read-from-subprocess-stdout-and-stderr-separately-while-preserving-order
- - https://stackoverflow.com/questions/12270645/can-you-make-a-python-subprocess-output-stdout-and-stderr-as-usual-but-also-cap
-"""
 import io
 import logging
 import signal
 import sys
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE
 from threading import Thread
 from typing import Union, Optional
 
@@ -43,18 +38,20 @@ class ProgramExecution(OutputExecution):
     def execute(self) -> ExecutionState:
         if not self._stopped and not self._interrupted:
             stdout = PIPE if self.read_output else None
-            stderr = STDOUT if self.read_output else None
+            stderr = PIPE if self.read_output else None
             try:
                 self._popen = Popen(" ".join(self.args) if USE_SHELL else self.args, stdout=stdout, stderr=stderr,
                                     shell=USE_SHELL)
-                output_reader = None
+                stdout_reader = None
+                stderr_reader = None
                 if self.read_output:
-                    output_reader = Thread(target=self._read_output, name='Output-Reader', daemon=True)
-                    output_reader.start()
+                    stdout_reader = self._start_output_reader(self._popen.stdout, False)
+                    stderr_reader = self._start_output_reader(self._popen.stderr, True)
 
                 self._popen.wait()
-                if output_reader:
-                    output_reader.join(timeout=1)
+                if self.read_output:
+                    stdout_reader.join(timeout=1)
+                    stderr_reader.join(timeout=1)
                 if self.ret_code == 0:
                     return ExecutionState.COMPLETED
             except FileNotFoundError as e:
@@ -66,6 +63,12 @@ class ProgramExecution(OutputExecution):
         if self._stopped or self.ret_code < 0:  # Negative exit code means terminated by a signal
             return ExecutionState.STOPPED
         raise ExecutionError("Process returned non-zero code " + str(self.ret_code), ExecutionState.FAILED)
+
+    def _start_output_reader(self, infile, is_err):
+        name = 'Stderr-Reader' if is_err else 'Stdout-Reader'
+        t = Thread(target=self._process_output, args=(infile, is_err), name=name, daemon=True)
+        t.start()
+        return t
 
     @property
     def status(self):
@@ -93,12 +96,13 @@ class ProgramExecution(OutputExecution):
     def remove_output_observer(self, observer):
         self._output_observers.remove(observer)
 
-    def _read_output(self):
-        for line in io.TextIOWrapper(self._popen.stdout, encoding="utf-8"):
-            line_stripped = line.rstrip()
-            self._status = line_stripped
-            print(line_stripped)
-            self._notify_output_observers(line_stripped, False)
+    def _process_output(self, infile, is_err):
+        with infile:
+            for line in io.TextIOWrapper(infile, encoding="utf-8"):
+                line_stripped = line.rstrip()
+                self._status = line_stripped
+                print(line_stripped, file=sys.stderr if is_err else sys.stdout)
+                self._notify_output_observers(line_stripped, is_err)
 
     def _notify_output_observers(self, output, is_error):
         for observer in self._output_observers:
