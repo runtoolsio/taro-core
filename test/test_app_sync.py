fd8697c4d0a4e303f695a593036777d922a6414f
Stan Svec <stan@taggstar.com>
2023-05-03 11:28:45 +0700

    serial test

---
+++ test/test_app_sync.py
@@ -0,0 +1,12 @@
+from taro import ExecutionState
+from test.taro_test_util import run_app_as_process_and_wait, run_app_as_process, run_app, StateWaiter
+
+
+def test_serial():
+    run_app_as_process_and_wait('exec -mc --id j1 --pending p --serial sleep 3',
+                                wait_for=ExecutionState.PENDING, daemon=True)
+    waiter = StateWaiter()
+    run_app_as_process('exec -mc --id j1 --serial echo waiting is over', state_queue=waiter.state_queue)
+    waiter.wait_for_state(ExecutionState.WAITING, timeout=1)
+    run_app('release p')
+    waiter.wait_for_state(ExecutionState.COMPLETED, timeout=1)
