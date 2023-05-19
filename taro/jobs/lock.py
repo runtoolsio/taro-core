7a8a4d66e71f74a4511e023d59d3a6dffe032e5d
Stan Svec <stan@taggstar.com>
2023-05-19 17:01:58 +0700

    #85: debug state lock times

--- taro/jobs/lock.py
+++ taro/jobs/lock.py
@@ -1,18 +1,20 @@
 import contextlib
+import logging
+import time
 from abc import ABC, abstractmethod
 
 import portalocker
 
 from taro import paths
 
+log = logging.getLogger(__name__)
+
 
 class StateLocker(ABC):
 
     @abstractmethod
-    def __call__(self, *args, **kwargs):
+    def __call__(self):
         """
-        :param args: none
-        :param kwargs: none
         :return: state lock
         """
 
@@ -33,18 +35,34 @@ class PortalockerStateLocker(StateLocker):
         self.timeout = timeout
 
     @contextlib.contextmanager
-    def __call__(self, *args, **kwargs):
+    def __call__(self):
+        start_time = time.time()
         with portalocker.Lock(self.lock_file, timeout=self.timeout) as lf:
-            yield PortalockerStateLock(lf)
+            elapsed_time_ms = (time.time() - start_time) * 1000
+            log.debug(f'event=[state_lock_acquired] wait=[{elapsed_time_ms:.2f} ms]')
+
+            lock = PortalockerStateLock(lf)
+            yield lock
+            if not lock.unlocked_at:
+                lock.unlocked_at = time.time()
+                lock.log_released()
 
 
 class PortalockerStateLock(StateLock):
 
     def __init__(self, locked_file):
         self.locked_file = locked_file
+        self.created_at = time.time()
+        self.unlocked_at = None
 
     def unlock(self):
         portalocker.unlock(self.locked_file)
+        self.unlocked_at = time.time()
+        self.log_released()
+
+    def log_released(self):
+        lock_time_ms = (self.unlocked_at - self.created_at) * 1000
+        log.debug(f'event=[state_lock_released] locked=[{lock_time_ms:.2f} ms]')
 
 
 class NullStateLocker(StateLocker, StateLock):
