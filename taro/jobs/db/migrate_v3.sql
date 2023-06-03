567ff26a6bbb866ba133a77eff1bff041e2b54dc
Stan Svec <stan@taggstar.com>
2023-06-03 09:33:21 +0700

    #79: migration for new exec_time column in sqlite

--- taro/jobs/db/migrate_v3.sql
+++ taro/jobs/db/migrate_v3.sql
@@ -2,4 +2,61 @@ BEGIN TRANSACTION;
 
 ALTER TABLE your_table_name ADD COLUMN misc TEXT;
 
+ALTER TABLE history RENAME TO old_history;
+
+CREATE TABLE history
+(
+  job_id text,
+  instance_id text,
+  created timestamp,
+  ended timestamp,
+  exec_time real,
+  state_changes text,
+  terminal_state text,
+  tracking text,
+  result text,
+  error_output text,
+  warnings text,
+  error text,
+  user_params text,
+  parameters text,
+  misc text
+);
+
+INSERT INTO history
+(
+  job_id,
+  instance_id,
+  created,
+  ended,
+  state_changes,
+  terminal_state,
+  tracking,
+  result,
+  error_output,
+  warnings,
+  error,
+  user_params,
+  parameters,
+  misc
+)
+SELECT
+  job_id,
+  instance_id,
+  created,
+  ended,
+  state_changes,
+  terminal_state,
+  tracking,
+  result,
+  error_output,
+  warnings,
+  error,
+  user_params,
+  parameters,
+  misc
+FROM old_history;
+
+DROP TABLE old_history;
+
 COMMIT;
